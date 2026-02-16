2936c30fc470d877c2486394fda1ebcc152d3f1d
immortale-g <twitch.immortaleg@gmail.com>
2026-02-16 18:21:21 +0100

    refactor: extract resolver factory

---
+++ src/main/java/com/thesettler_x_create/minecolonies/building/ShopResolverFactory.java
@@ -0,0 +1,63 @@
+package com.thesettler_x_create.minecolonies.building;
+
+import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableList;
+import com.minecolonies.api.colony.requestsystem.factory.IFactoryController;
+import com.minecolonies.api.colony.requestsystem.location.ILocation;
+import com.minecolonies.api.colony.requestsystem.resolver.IRequestResolver;
+import com.minecolonies.api.colony.requestsystem.token.IToken;
+import com.minecolonies.api.util.constant.TypeConstants;
+import com.minecolonies.core.colony.requestsystem.resolvers.DeliveryRequestResolver;
+import com.minecolonies.core.colony.requestsystem.resolvers.PickupRequestResolver;
+import com.thesettler_x_create.TheSettlerXCreate;
+import com.thesettler_x_create.minecolonies.requestsystem.resolver.CreateShopRequestResolver;
+
+/** Builds resolver list for the Create Shop. */
+final class ShopResolverFactory {
+  private final BuildingCreateShop shop;
+
+  ShopResolverFactory(BuildingCreateShop shop) {
+    this.shop = shop;
+  }
+
+  ImmutableCollection<IRequestResolver<?>> createResolvers(
+      ImmutableCollection<IRequestResolver<?>> baseResolvers) {
+    ImmutableList.Builder<IRequestResolver<?>> builder = ImmutableList.builder();
+    for (IRequestResolver<?> resolver : baseResolvers) {
+      if (resolver
+          instanceof
+          com.minecolonies.core.colony.requestsystem.resolvers.core
+              .AbstractWarehouseRequestResolver) {
+        // CreateShop is not a BuildingWareHouse; avoid MineColonies' warehouse resolver cast crash.
+        continue;
+      }
+      builder.add(resolver);
+    }
+
+    ILocation location = shop.getRequester().getLocation();
+    IFactoryController factory = shop.getColony().getRequestManager().getFactoryController();
+    IToken<?> token = factory.getNewInstance(TypeConstants.ITOKEN);
+
+    CreateShopRequestResolver shopResolver = new CreateShopRequestResolver(location, token);
+    builder.add(shopResolver);
+    IToken<?> deliveryResolverToken = factory.getNewInstance(TypeConstants.ITOKEN);
+    IToken<?> pickupResolverToken = factory.getNewInstance(TypeConstants.ITOKEN);
+    builder.add(new DeliveryRequestResolver(location, deliveryResolverToken));
+    builder.add(new PickupRequestResolver(location, pickupResolverToken));
+
+    shop.setResolverState(shopResolver, deliveryResolverToken, pickupResolverToken);
+
+    if (BuildingCreateShop.isDebugRequests()) {
+      TheSettlerXCreate.LOGGER.info(
+          "[CreateShop] createResolvers at {} -> {}",
+          shop.getLocation().getInDimensionLocation(),
+          builder.build().size());
+      TheSettlerXCreate.LOGGER.info(
+          "[CreateShop] delivery resolver token={} pickup resolver token={}",
+          deliveryResolverToken,
+          pickupResolverToken);
+    }
+
+    return builder.build();
+  }
+}
